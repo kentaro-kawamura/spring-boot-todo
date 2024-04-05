8f835f8c6b00fd4a9c65d93dcb8a4e0d8fe05c61
kentaro-kawamura <suppersoccer008@gmai.com>
2024-04-05 23:51:27 +0900

    Create DB Tableand Visible browser

--- src/main/kotlin/com/kawamura/todo/model/Todo.kt
+++ src/main/kotlin/com/kawamura/todo/model/Todo.kt
@@ -3,9 +3,11 @@ package com.kawamura.todo.model
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import java.time.LocalDateTime
 
 @Entity
+@Table(name="tododb")
 data class Todo(
     var title: String? = null,
     var status: String = "todo",
